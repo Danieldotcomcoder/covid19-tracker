74fc719657f76f2711dbdbfb96f3aeea510eb05b
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2021-12-21 21:31:31 +0200

    Add: Details and Item component

---
+++ src/Components/Details.js
@@ -0,0 +1,76 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import { fetchCountry } from '../Redux/countries/countries';
+import img from '../assets/virus.png';
+import leftarrow from '../assets/back-button.png';
+import micimg from '../assets/microphone.png';
+import settingimg from '../assets/settings.png';
+import rightarrow from '../assets/right.png';
+
+const Details = () => {
+  const { name } = useParams();
+  const dispatch = useDispatch();
+  const { country, loading } = useSelector((state) => ({
+    loading: state.loadingBar.default,
+    country: state.countries.selected,
+  }));
+
+  useEffect(() => {
+    dispatch(fetchCountry(name));
+  }, []);
+
+  if (loading || !country) {
+    return null;
+  }
+  const { All } = country;
+  const list = Object.entries(country).slice(1);
+
+  return (
+    <section>
+      <header className="App-header">
+        <Link to="/">
+          <img src={leftarrow} alt="arrowimage" />
+        </Link>
+        <h5 className="App-header-title">town/city views</h5>
+        <img src={micimg} alt="arrowimage" />
+        <div className="pl-5">
+          <img src={settingimg} alt="arrowimage" />
+        </div>
+      </header>
+      <div className="Details-banner">
+        <div className="Details-banner-left">
+          <img src={img} alt="map view" className="App-map" />
+        </div>
+        <div className="Details-banner-right">
+          <h1 className="App-title">{All.country}</h1>
+          <p className="App-subtitle">
+            {All.confirmed}
+             &nbsp;
+            Cases
+          </p>
+        </div>
+      </div>
+      <section className="Home-stats">
+        <h5 className="App-section-title">CITY/TOWN BREAKDOWN - 2021</h5>
+        <ul>
+          {list.map(([name, { confirmed }]) => (
+            <li key={name} className="Details-item">
+              <h6 className="Details-item-title">{name}</h6>
+              <div className="Details-item-right">
+                <p className="App-subtitle">
+                  {confirmed}
+                  {' '}
+                  cases
+                </p>
+                <img src={rightarrow} alt="arrowimage" />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </section>
+  );
+};
+
+export default Details;
