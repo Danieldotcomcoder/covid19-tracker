110713dcdfeb6ac33f509ad835e2ba1c66f9e607
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-01-07 13:11:59 +0200

    Fix an error where the page crashes after refresh

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -14,7 +14,6 @@ import rightarrow from '../assets/right.png';
 
 const Details = () => {
   const country = useSelector((state) => state.countries.current);
-  const [info, setInfo] = useState();
   const { name } = useParams();
   const dispatch = useDispatch();
 
@@ -22,39 +21,45 @@ const Details = () => {
     dispatch(fetchData());
   }, []);
   const data = country.find((item) => item.country === name);
+  if (data && country) {
+    return (
+      <section>
+        <header className="home-main">
+          <Link to="/">
+            <img src={leftarrow} alt="arrowimage" />
+          </Link>
+          <h5 className="main-title">More Information</h5>
+          <img src={micimg} alt="arrowimage" className="mic-image" />
+          <div className="pl-5">
+            <img src={settingimg} alt="arrowimage" />
+          </div>
+        </header>
+        <div className="banner">
+          <div className="banner-left">
+            <img src={img} alt="map view" className="App-map" />
+          </div>
+          <div className="banner-right">
+            <h2 className="Details-title" />
+            <p className="App-subtitle">
 
-  return (
-    <section>
-      <header className="home-main">
-        <Link to="/">
-          <img src={leftarrow} alt="arrowimage" />
-        </Link>
-        <h5 className="main-title">More Information</h5>
-        <img src={micimg} alt="arrowimage" className="mic-image" />
-        <div className="pl-5">
-          <img src={settingimg} alt="arrowimage" />
+              &nbsp;
+              Cases
+            </p>
+          </div>
         </div>
-      </header>
-      <div className="banner">
-        <div className="banner-left">
-          <img src={img} alt="map view" className="App-map" />
-        </div>
-        <div className="banner-right">
-          <h2 className="Details-title" />
-          <p className="App-subtitle">
-
-            &nbsp;
-            Cases
-          </p>
-        </div>
-      </div>
-      <section className="Home-stats">
-        <h5 className="App-section-title">Details</h5>
-        <ul>
-          <li>{data.country}</li>
-        </ul>
+        <section className="Home-stats">
+          <h5 className="App-section-title">Details</h5>
+          <ul>
+            <li>{data.country}</li>
+          </ul>
+        </section>
       </section>
-    </section>
+    );
+  }
+  return (
+    <div>
+      Fetching Data ...
+    </div>
   );
 };
 
